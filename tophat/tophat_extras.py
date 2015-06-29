9a4a053659208d51e2e4ba6ea3260f4f135e8cad
Kyle Mc <mbio.kyle@gmail.com>
2015-06-29 12:16:30 -0500

    Refactored extras as a class, implemented gene annotations

--- tophat/tophat_extras.py
+++ tophat/tophat_extras.py
@@ -2,144 +2,207 @@ import os, glob, re, subprocess, json, time
 from jsonschema import validate
 import smtplib
 from email.mime.text import MIMEText
+import sqlite3
 
+class TophatExtras():
 
-# not a ruffus command
-def check_default_args(cores,index,output, log):
-    
-    def_opts = {
-        "cores":10,
-        "index":"/data/refs/hg19/hg19", 
-        "output":"./"
-    }
+    def __init__(self, log):
+        self.log = log
+        self.conn = None
 
-    running_opts = {
-        "cores":cores,
-        "index":index, 
-        "output":output
-    }
+    # not a ruffus command
+    def check_default_args(self, cores, index, output):
+        
+        def_opts = {
+            "cores":10,
+            "index":"/data/refs/hg19/hg19", 
+            "output":"./"
+        }
 
-    for key in def_opts.keys():
-        if def_opts[key] == running_opts[key]:
-            log.warn(" {} parameter was not given using default: {}".format(key, def_opts[key]))
+        running_opts = {
+            "cores":cores,
+            "index":index, 
+            "output":output
+        }
 
-# not a ruffus task
-def make_fastq_list(directory, log):
-    
-    # make sure the dir exists
-    if not os.path.isdir(directory):
-        log.warn( "%s is not a valid dir, exiting", directory)
-        raise SystemExit 
+        for key in def_opts.keys():
+            if def_opts[key] == running_opts[key]:
+                self.log.warn(" {} parameter was not given using default: {}".format(key, def_opts[key]))
 
-    # see if there are any compressed files
-    gz_blob = os.path.join(directory, "*.fastq.gz")
-    gzs = glob.glob(gz_blob)
+    # not a ruffus task
+    def make_fastq_list(self, directory):
+        
+        # make sure the dir exists
+        if not os.path.isdir(directory):
+            self.log.warn( "%s is not a valid dir, exiting", directory)
+            raise SystemExit 
 
-    for gz in gzs:
-        log.info("gunzipping %s", gz)   
+        # see if there are any compressed files
+        gz_blob = os.path.join(directory, "*.fastq.gz")
+        gzs = glob.glob(gz_blob)
 
-        # 0 == all good, and bool false
-        if subprocess.call(["gunzip",gz]):
-            log.warn("gunzipping %s failed, exiting", gz)
+        for gz in gzs:
+            self.log.info("gunzipping %s", gz)   
+
+            # 0 == all good, and bool false
+            if subprocess.call(["gunzip",gz]):
+                self.log.warn("gunzipping %s failed, exiting", gz)
+                raise SystemExit
+
+        # now glob the fastqs
+        blob = os.path.join(directory, "*.fastq")
+        fastqs = [os.path.abspath(fastq) for fastq in glob.glob(blob)]
+
+        # make sure we got stuff
+        if not fastqs:
+            self.log.warn("Fastq list is empty, exiting")
             raise SystemExit
 
-    # now glob the fastqs
-    blob = os.path.join(directory, "*.fastq")
-    fastqs = [os.path.abspath(fastq) for fastq in glob.glob(blob)]
+        return fastqs
 
-    # make sure we got stuff
-    if not fastqs:
-        log.warn("Fastq list is empty, exiting")
-        raise SystemExit
+    # not a ruffus task
+    def process_de_conf(self, file):
 
-    return fastqs
+        # make sure the file is there
+        if not os.path.isfile(file):
+            self.log.warn( "%s is not a valid file, exiting", file)
+            raise SystemExit 
 
-# not a ruffus task
-def process_de_conf(file, log):
-
-    # make sure the file is there
-    if not os.path.isfile(file):
-        log.warn( "%s is not a valid file, exiting", file)
-        raise SystemExit 
-
-    # schema to validate against
-    de_conf_schema = {
-        "type": "object",
-        "properties": {
-            "negative-condition": {
-                "type":"object",
-                "properties": {
-                    "label": {"type": "string"},
-                    "files": {"type": "array"}
-                }
-            },
-            "positve-condition": {
-                "type":"object",
-                "properties": {
-                    "label": {"type": "string"},
-                    "files": {"type": "array"}
+        # schema to validate against
+        de_conf_schema = {
+            "type": "object",
+            "properties": {
+                "negative-condition": {
+                    "type":"object",
+                    "properties": {
+                        "label": {"type": "string"},
+                        "files": {"type": "array"}
+                    }
+                },
+                "positve-condition": {
+                    "type":"object",
+                    "properties": {
+                        "label": {"type": "string"},
+                        "files": {"type": "array"}
+                    }
                 }
             }
         }
-    }
-    with open(file) as json_data:
-        conf = json.load(json_data)
-        
-        # make sure it matches
-        try:
-            validate(conf, de_conf_schema)
+        with open(file) as json_data:
+            conf = json.load(json_data)
+            
+            # make sure it matches
+            try:
+                validate(conf, de_conf_schema)
 
-        except ValidationError:
-            log.warn("%s is not valid DE conf json", conf)
+            except ValidationError:
+                self.log.warn("%s is not valid DE conf json", conf)
+                raise SystemExit
+
+            # if its good return it
+            return conf
+
+
+    # not a ruffus task
+    # send email
+    # the plan is to report the specific error
+    # and also append the the contents of the log for good measure
+    def report_error(self, task, message):
+
+        # Create a text/plain message
+        email_body = []
+        email_body.append("Hello, Kyle\n")
+        email_body.append("{} task failed with the following error: ".format(task))
+        email_body.append(message)
+
+        # grab the log file name from the log
+        # we add the file handler first
+        # so its here
+        log_file = self.log.handlers[0].baseFilename
+        email_body.append("\n#######################################################")
+        email_body.append("#                PIPELINE LOG                         #")
+        email_body.append("#######################################################")
+        with open(log_file, "r") as log:
+            for line in log:
+                email_body.append(line.rstrip())
+
+        msg = MIMEText("\n".join(email_body))
+
+        # header stuff
+        # no one else cares but me!
+        root  = "root@alpha-helix.oncology.wisc.edu"
+        me = "kgmcchesney@wisc.edu"
+        subject = "Tophat pipeline failure report: {}".format(time.strftime("%d/%m/%Y"))
+        
+        msg['Subject'] = subject
+        msg['From'] = root
+        msg['To'] = me
+        
+        # Send the message via our own SMTP server, but don't include the
+        # envelope header.
+        s = smtplib.SMTP('localhost')
+        s.sendmail(root, [me], msg.as_string())
+        s.quit()
+
+    def annotate_gene(self, gene):
+
+        # double checking!
+        if self.conn is None:
+            return "N/A"
+
+        c = self.conn.cursor()
+        c.execute("SELECT desc FROM geneAnnot WHERE name=?", [gene])
+
+        gene = c.fetchone()
+        
+        if gene is not None:
+            gene = gene[0].rstrip()
+            return gene
+
+        else:
+            return "N/A"
+
+    def make_annotation_db(self, tsv):
+
+        # check it exists, read it and make the db
+        if not os.path.isfile(tsv):
+            self.log.warn("%s is not a annotation file!", tsv)
             raise SystemExit
 
-        # if its good return it
-        return conf
+        # open up the DB
+        name = os.path.splitext(os.path.basename(tsv))[0]
+        self.conn = sqlite3.connect("./{}_annotations.sqlite".format(name))
 
+        genes = []
+        with open(tsv, "r") as data:
+            for line in data:
+                values = line.split("\t")
 
-# not a ruffus task
-# send email
-# the plan is to report the specific error
-# and also append the the contents of the log for good measure
-def report_error(task, message, log):
+                # make sure it looks good
+                if len(values) != 3:
+                    self.log.warn("Invalid entry in gene annotation file")
+                    continue
 
+                # append to genes array
+                genes.append((values[0], values[1], values[2]))
 
-    # Create a text/plain message
-    email_body = []
-    email_body.append("Hello, Kyle\n")
-    email_body.append("{} task failed with the following error: ".format(task))
-    email_body.append(message)
+        # make sure at least one is good
+        if len(genes) == 0:
+            self.log.warn("No valid genes found, exiting!")
+            raise SystemExit
 
-    # grab the log file name from the log
-    # we add the file handler first
-    # so its here
-    log_file = log.handlers[0].baseFilename
-    email_body.append("\n#######################################################")
-    email_body.append("#                PIPELINE LOG                         #")
-    email_body.append("#######################################################")
-    with open(log_file, "r") as log:
-        for line in log:
-            email_body.append(line.rstrip())
+        # create the table
+        c = self.conn.cursor()
+        c.execute("""CREATE TABLE geneAnnot 
+                    (id INT PRIMARY KEY NOT NULL, 
+                    name CHAR(50) NOT NULL, desc TEXT NOT NULL)
+                  """)
+        c.executemany("INSERT INTO geneAnnot VALUES (?,?,?)", genes)
+        self.conn.commit()
 
-    msg = MIMEText("\n".join(email_body))
-
-    # header stuff
-    # no one else cares but me!
-    root  = "root@alpha-helix.oncology.wisc.edu"
-    me = "kgmcchesney@wisc.edu"
-    subject = "Tophat pipeline failure report: {}".format(time.strftime("%d/%m/%Y"))
-    
-    msg['Subject'] = subject
-    msg['From'] = root
-    msg['To'] = me
-    
-    # Send the message via our own SMTP server, but don't include the
-    # envelope header.
-    s = smtplib.SMTP('localhost')
-    s.sendmail(root, [me], msg.as_string())
-    s.quit()
-
-
-
-def annotate_gene(gene):
-    return "ANOOTATED"+gene
+    def db_init(self, db):
+        try:
+            self.conn = sqlite3.connect(db)
+        except Exception:
+            self.log.warn("%s is not a valid sqlite db!", db)
+            raise SystemExit
--- tophat/tophat_pipeline.py
+++ tophat/tophat_pipeline.py
@@ -11,7 +11,7 @@ from ruffus import *
 import ruffus.cmdline as cmdline
 
 # custom functions
-from tophat_extras import *
+from tophat_extras import TophatExtras
 
 # system imports
 import subprocess, logging, os, re, time
@@ -27,11 +27,11 @@ from email.MIMEText import MIMEText
 from email.Utils import COMMASPACE, formatdate
 from email import Encoders
 
-parser = cmdline.get_argparse(description='Given a directory of NON-paired end reads -- Align them with tophat and generate wigs')
+parser = cmdline.get_argparse(description='This pipeline provides a number of funtionalities for working with RNAseq data')
 
 # Program arguments
 parser.add_argument("--dir", help="Fullpath to the directory where the FASTQ reads are located", required=True)
-parser.add_argument("--cores", help="Number of cores to run bowtie on", default=10)
+parser.add_argument("--cores", help="Number of cores to run bowtie on", default='10')
 parser.add_argument("--index", help="Fullpath to the bowtie2 index in: /full/file/path/basename form", default="/data/refs/hg19/hg19")
 parser.add_argument("--output", help="Fullpath to output directory", default="./")
 parser.add_argument("--size", help="Fullpath to size file", required=True)
@@ -43,6 +43,8 @@ parser.add_argument("--wig", help="Whether or not wig files should be generated"
 parser.add_argument("--one-codex", help="Whether or not to upload each sample to one codex for metagenomic analysis", default=False)
 parser.add_argument("--de", help="Whether or not differential expression should be calculated", type=bool, default=False)
 parser.add_argument("--de-conf", help="fullpath to differential expresssion configuration file")
+parser.add_argument("--annotation-db", help="fullpath to the sqlite db file, <id><name><desc>")
+parser.add_argument("--annotation-file", help="fullpath to a tsv file of gene annotations, will create sqlite db")
 
 # reporting
 parser.add_argument("--emails", help="Emails to send DE results too", default="kgmcchesney@wisc.edu", nargs="+")
@@ -74,24 +76,34 @@ stream_handler.setFormatter(log_formatter)
 # set it all up
 log.addHandler(file_handler)
 log.addHandler(stream_handler)
-
 log.info("Starting Tophat/DE Run")
 
 # pre checking
-check_default_args(options.cores, options.index, options.output, log)
-input_files = make_fastq_list(options.dir, log)
+# create extas instance
+extras = TophatExtras(log)
+
+# checking the annotation situation :)
+# we either got a db, got a db file and need to make a db, or do nothing
+if options.annotation_file:
+    extras.make_annotation_db(options.annotation_file)
+
+elif options.annotation_db:
+    extras.init_db(options.annotation_db)
+
+extras.check_default_args(options.cores, options.index, options.output)
+input_files = extras.make_fastq_list(options.dir)
 genome = os.path.splitext(os.path.basename(options.index))[0]
 
 @active_if(options.one_codex)
-@transform(input_files, suffix(".fastq"), ".fastq")
-def upload_to_one_codex(input_file, output_file):
+@transform(input_files, suffix(".fastq"), ".fastq", extras)
+def upload_to_one_codex(input_file, output_file, extras):
 
     args = ["onecodex", "upload", input_file]
 
     try:
         output = subprocess.check_output(args, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError:
-        report_error("One Codex","uploading to One Codex failed \n{}".format(output), log)
+        extras.report_error("One Codex","uploading to One Codex failed \n{}".format(output))
         raise SystemExit
 
 @active_if(options.paired)
@@ -101,8 +113,8 @@ def collate_files(input_files, output_files):
 
 # paired alignment
 @active_if(options.paired)
-@transform(collate_files, formatter("([^/]+)_[12].fastq$"), options.output+"{1[0]}-tophat-results/accepted_hits.bam", options)
-def tophat_align_paired(input_files, output_file, options):
+@transform(collate_files, formatter("([^/]+)_[12].fastq$"), options.output+"{1[0]}-tophat-results/accepted_hits.bam", options, extras)
+def tophat_align_paired(input_files, output_file, options, extras):
 
     # we cant to have tophat write results to output+filename
     output = os.path.dirname(output_file)
@@ -111,7 +123,7 @@ def tophat_align_paired(input_files, output_file, options):
     try:
         output = subprocess.check_output(args, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError:
-        report_error("tophat_paired","tophat2 paired run failed:\n{}".format(output), log)
+        extras.report_error("tophat_paired","tophat2 paired run failed:\n{}".format(output))
         raise SystemExit
 
     # print output
@@ -120,18 +132,18 @@ def tophat_align_paired(input_files, output_file, options):
 
 # unpaired alignment function
 @active_if(not options.paired)
-@transform(input_files, formatter(), options.output+"{basename[0]}-tophat-results/accepted_hits.bam", options)
-def tophat_align_unpaired(input_file, output_file, options):
+@transform(input_files, formatter(), options.output+"{basename[0]}-tophat-results/accepted_hits.bam", options, extras)
+def tophat_align_unpaired(input_file, output_file, options, extras):
 
     # we cant to have tophat write results to output+filename
     output = os.path.dirname(output_file)
-    args = ["tophat2", "-G", options.gtf,"-p", str(options.cores), "-o", output, options.index, input_file]
+    args = ["tophat2", "-G", options.gtf,"-p", options.cores, "-o", output, options.index, input_file]
 
     try:
         output = subprocess.check_output(args, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError:
         log.warn(output)
-        report_error("tophat_unpaird","tophat2 unpaired failed: \n{}".format(output), log)
+        extras.report_error("tophat_unpaird","tophat2 unpaired failed: \n{}".format(output))
         raise SystemExit
 
     # print output
@@ -139,19 +151,19 @@ def tophat_align_unpaired(input_file, output_file, options):
     log.info(output)
 
 # get rid of stupid accepted_hits.bam file
-@transform([tophat_align_unpaired, tophat_align_paired], 
+@transform([tophat_align_unpaired, tophat_align_paired],
             formatter(r".*/([^/]+)-tophat-results/accepted_hits.bam$"), 
-            ''.join([options.output, "{1[0]}", ".bam"]) )
-def rename_accepted_hits(input_file, output_file):
+            ''.join([options.output, "{1[0]}", ".bam"]), extras )
+def rename_accepted_hits(input_file, output_file, extras):
 
     try:
         os.rename(input_file, output_file)
     except OSError:
-        report_error("rename_accepted_hits","Renaming {} to {} failed".format(input_file, output_file),log)
+        extras.report_error("rename_accepted_hits","Renaming {} to {} failed".format(input_file, output_file),log)
 
 # both the tophat functions can feed into here
-@transform(rename_accepted_hits, suffix(".bam"),".sorted.bam")
-def sort_bam(input_file, output_file):
+@transform(rename_accepted_hits, suffix(".bam"),".sorted.bam", extras)
+def sort_bam(input_file, output_file, extras):
     log.info("Sorting %s ", input_file)
 
     # hacky
@@ -159,20 +171,20 @@ def sort_bam(input_file, output_file):
 
     if subprocess.call(["samtools-rs", "rocksort", "-@", "8", "-m", "16G", input_file, output_file]):
         log.warn("bam sorting %s failed, exiting", input_file)
-        report_error("sort_bam","bam sorting {} failed".format(input_file), log)
+        extras.report_error("sort_bam","bam sorting {} failed".format(input_file))
         raise SystemExit
     
     log.info("Deleting old file %s", input_file)
     os.unlink(input_file)
 
 @active_if(options.wig)
-@transform(sort_bam, suffix(".sorted.bam"), ".bed", options.output)
-def bam_to_bed(input_file, output_file, output):
+@transform(sort_bam, suffix(".sorted.bam"), ".bed", options.output, extras)
+def bam_to_bed(input_file, output_file, output, extras):
 
     log.info("Converting %s to a bed file", input_file)
     if subprocess.call("bamToBed -i {} > {}".format(input_file, output_file), shell=True):
         log.warn("bam to bed conversion of %s failed, exiting", input_file)
-        report_error("bam_to_bed","bam to bed conversion of {} failed".format(input_file), log)
+        extras.report_error("bam_to_bed","bam to bed conversion of {} failed".format(input_file))
         raise SystemExit
 
     # now we can move sorted bam to output
@@ -181,14 +193,14 @@ def bam_to_bed(input_file, output_file, output):
     os.rename(input_file, new_name)
 
 @active_if(options.wig)
-@transform(bam_to_bed, suffix(".bed"), ".cov", options.size)
-def bed_to_cov(input_file, output_file,  size_file):
+@transform(bam_to_bed, suffix(".bed"), ".cov", options.size, extras)
+def bed_to_cov(input_file, output_file,  size_file, extras):
     log.info("Converting %s to a genome coverage file", input_file)
     
     command = "genomeCoverageBed -d -i {} -g {} > {}".format( input_file, size_file, output_file)
     if subprocess.call(command, shell=True):
         log.warn("bed to coverage conversion of %s failed, exiting", input_file)
-        report_error("bed_to_cov","bed to cov conversion of {} failed".format(input_file), log)
+        extras.report_error("bed_to_cov","bed to cov conversion of {} failed".format(input_file))
         raise SystemExit
 
     log.info("Deleting old file %s", input_file)
@@ -226,11 +238,11 @@ def cov_to_wig(input_file, output_file, genome, output):
     os.rename(output_file, new_name)
 
 @active_if(options.de)
-@merge(sort_bam, os.path.join(options.output,"cuffdiff/gene_exp.diff",), options)
-def run_cuffdiff(input_files, output_file, options):
+@merge(sort_bam, os.path.join(options.output,"cuffdiff/gene_exp.diff",), options, extras)
+def run_cuffdiff(input_files, output_file, options, extras):
     
     # grab the conf
-    conf = process_de_conf(options.de_conf, log)
+    conf = extras.process_de_conf(options.de_conf)
 
     # make the output file
     output_dir = os.path.join(options.output,"cuffdiff/")
@@ -279,16 +291,17 @@ def run_cuffdiff(input_files, output_file, options):
     # make sure both groups got something and that the inputfiles is emtpy
     if len(neg_files) == 0 or len(pos_files) == 0 or len(input_files) != 0:
         log.warn("Cuffdiff file specification error!")
-        report_error("cuffdiff", "Cuffdiff file specification error!", log)
+        extras.report_error("cuffdiff", "Cuffdiff file specification error!")
         raise SystemExit
 
     # call it
+    log.info("Starting cuffdiff run")
     args = ["cuffdiff", "-o", output_dir, "-L", label_string, "-p", options.cores, options.gtf, ','.join(neg_files), ','.join(pos_files)]
     try:
         output = subprocess.check_output(args, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError:
         log.warn("Cuffdiff failed: %s", output)
-        report_error("cuffdiff","Cuffdiff failed: \n{}".format(output), log)
+        extras.report_error("cuffdiff","Cuffdiff failed: \n{}".format(output))
         raise SystemExit
 
     # print output
@@ -296,8 +309,8 @@ def run_cuffdiff(input_files, output_file, options):
     log.info(output)
 
 @active_if(options.de)
-@transform(run_cuffdiff, suffix(".diff"), ".xlsx", options)
-def write_excel_sheet(input_file, output_file, options):
+@transform(run_cuffdiff, suffix(".diff"), ".xlsx", options, extras)
+def write_excel_sheet(input_file, output_file, options, extras):
     
     # best message ever
     log.info("Writing gene expression results to spreadsheets")
@@ -312,7 +325,8 @@ def write_excel_sheet(input_file, output_file, options):
     df.columns = keep_cols
 
     # annotate the genes
-    df['gene_annotation'] = df['gene_name'].apply(annotate_gene)
+    if(options.annotation_db or options.annotation_file):
+        df['gene_annotation'] = df['gene_name'].apply(extras.annotate_gene)
 
     # sort sig to the top
     df.sort(columns="significant", ascending=False, inplace=True, kind="heapsort")
@@ -325,8 +339,8 @@ def write_excel_sheet(input_file, output_file, options):
     log.info("results written to: %s", output_file)
 
 @active_if(options.de)
-@transform(write_excel_sheet, suffix(".xlsx"), ".email", options, input_files)
-def report_success(input_file, output_file, options, inputfiles):
+@transform(write_excel_sheet, suffix(".xlsx"), ".email", options, input_files, extras)
+def report_success(input_file, output_file, options, inputfiles, extras):
     
     log.info("Sending email report")
     
