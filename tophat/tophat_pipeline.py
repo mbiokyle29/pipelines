bbd54783c1d07f54436219407d27c9977a2a440b
Kyle <mbio.kyle@gmail.com>
2015-08-26 10:56:05 -0500

    Removed all DE stuff to keep this only for alignment, sanity when adding to cuffdiff pipeline

--- tophat/tophat_pipeline.py
+++ tophat/tophat_pipeline.py
@@ -47,14 +47,6 @@ parser.add_argument("--trim-val", help="Value to trim by (from the end of read)"
 parser.add_argument("--bw", help="Whether or not big wig files should be generated", type=bool, default=False)
 parser.add_argument("--one-codex", help="Whether or not to upload each sample to one codex for metagenomic analysis", default=False)
 
-# DE
-parser.add_argument("--de", help="Whether or not differential expression should be calculated", type=bool, default=False)
-parser.add_argument("--de-conf", help="fullpath to differential expresssion configuration file")
-
-# gene annotations for DE
-parser.add_argument("--annotation-db", help="fullpath to the sqlite db file, <id><name><desc>")
-parser.add_argument("--annotation-file", help="fullpath to a tsv file of gene annotations, will create sqlite db")
-
 # reporting
 parser.add_argument("--emails", help="Emails to send DE results too", default="mbio.kyle@gmail.com", nargs="+")
 
@@ -85,32 +77,15 @@ stream_handler.setFormatter(log_formatter)
 # set it all up
 log.addHandler(file_handler)
 log.addHandler(stream_handler)
-log.info("Starting Tophat/DE Run")
+log.info("Starting Tophat")
 
 # pre checking
 # create extas instance
 extras = TophatExtras(log)
 
-# checking the annotation situation :)
-# we either got a db, got a db file and need to make a db, or do nothing
-if options.annotation_file:
-    extras.make_annotation_db(options.annotation_file, time_stamp, options.output)
-
-elif options.annotation_db:
-    extras.init_db(options.annotation_db)
-
 extras.check_default_args(options.cores, options.index, options.output)
 input_files = extras.make_fastq_list(options.dir)
 genome = os.path.splitext(os.path.basename(options.index))[0]
-
-# define these here
-neg_files = []
-pos_files = []
-conf = None
-
-# grab the conf
-if options.de:
-    conf = extras.process_de_conf(options.de_conf)
 
 @active_if(options.paired)
 @collate(input_files, formatter("([^/]+)_[12].fastq$"), ["{path[0]}/{1[0]}_1.fastq", "{path[0]}/{1[0]}_2.fastq"])
@@ -309,154 +284,6 @@ def bg_to_bw(input_file, output_file, genome, output):
 
     log.info("Deleting old file %s", input_file)
     os.unlink(input_file)
-
-@active_if(options.de)
-@merge(sort_bam, os.path.join(options.output,"cuffdiff/gene_exp.diff",), options, extras)
-def run_cuffdiff(input_files, output_file, options, extras):
-
-    # make the output file
-    output_dir = os.path.join(options.output,"cuffdiff/")
-
-    # get the label
-    neg_label = conf['negative-condition']['label']
-    pos_label = conf['positive-condition']['label']
-    label_string = "{},{}".format(neg_label, pos_label)
-
-    #neg_files = []
-    #pos_files = []
-
-    # match the files in conf to the bams we have
-    for conf_file in conf['negative-condition']['files']:
-
-        conf_basename = os.path.splitext(conf_file)[0]
-        # check the input files
-        for bam_file in input_files:
-            bam_basename = os.path.basename(bam_file).split(os.extsep)[0]
-            
-            # see if they match
-            if conf_basename == bam_basename:
-                log.info("%s matched %s in negative-condition", conf_file, bam_file)
-                neg_files.append(bam_file)
-
-                # remove it for optimization :) I think
-                input_files.remove(bam_file)
-
-     # match the files in conf to the bams we have
-    for conf_file in conf['positive-condition']['files']:
-        conf_basename = os.path.splitext(conf_file)[0]
-
-        # check the input files
-        for bam_file in input_files:
-            bam_basename = os.path.basename(bam_file).split(os.extsep)[0]
-
-            # see if they match
-            if conf_basename == bam_basename:
-                log.info("%s matched %s in positive-condition", conf_file, bam_file)
-                pos_files.append(bam_file)
-
-                # remove it for optimization :) I think
-                input_files.remove(bam_file)
-
-
-    # make sure both groups got something and that the inputfiles is emtpy
-    if len(neg_files) == 0 or len(pos_files) == 0 or len(input_files) != 0:
-        log.warn("Cuffdiff file specification error!")
-        extras.report_error("cuffdiff", "Cuffdiff file specification error!")
-        raise SystemExit
-
-    # call it
-    log.info("Starting cuffdiff run")
-    args = ["cuffdiff", "-o", output_dir, "-L", label_string, "-p", options.cores, options.gtf, ','.join(neg_files), ','.join(pos_files)]
-    try:
-        output = subprocess.check_output(args, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError:
-        log.warn("Cuffdiff failed: %s", output)
-        extras.report_error("cuffdiff","Cuffdiff failed: \n{}".format(output))
-        raise SystemExit
-
-    # print output
-    log.info("cuffdiff output:")
-    log.info(output)
-
-@active_if(options.de)
-@transform(run_cuffdiff, suffix(".diff"), ".xlsx", options, extras)
-def write_excel_sheet(input_file, output_file, options, extras):
-    
-    # best message ever
-    log.info("Writing gene expression results to spreadsheets")
-    keep_cols = ["test_id", "locus", "sample_1", "sample_2", "status", "value_1",
-                "value_2", "log2(fold_change)", "test_stat", "p_value", "q_value", "significant"]
-
-    # read it in
-    df = pd.read_table(input_file, sep="\t", usecols=keep_cols)
-
-    # change the na,e of test_id
-    keep_cols[0] = "gene_name"
-    df.columns = keep_cols
-
-    # annotate the genes
-    if(options.annotation_db or options.annotation_file):
-        df['gene_annotation'] = df['gene_name'].apply(extras.annotate_gene)
-
-    # sort sig to the top
-    df.sort(columns="significant", ascending=False, inplace=True, kind="heapsort")
-
-    # write the sucker to a file
-    writer = pd.ExcelWriter(output_file)
-    df.to_excel(writer, sheet_name="Sheet1", index=False)
-    writer.save()
-
-    log.info("results written to: %s", output_file)
-
-
-@active_if(options.de)
-@transform(write_excel_sheet, suffix(".xlsx"), ".email", options, input_files, extras)
-def report_success(input_file, output_file, options, inputfiles, extras):
-    
-    log.info("Sending email report")
-    
-    # Create a text/plain message
-    email_body = []
-    email_body.append("Differential expression pipeline results:\n")
-    email_body.append("The following bam files were compared:")
-    
-    email_body.append("negative condition:")
-    for file in neg_files:
-        email_body.append("- {}".format(file))
-
-    email_body.append("positive condition:")
-    for file in pos_files:
-        email_body.append("- {}".format(file))
-
-    email_body.append("\nThe results (xlsx spreadsheet) and pipeline log are attatched")
-    email_body.append("Please direct any questions to kgmcchesney@wisc.edu")
-
-    # msg object
-    msg = MIMEMultipart()
-
-    # header stuff
-    # no one else cares but me!
-    root  = "root@alpha-helix.oncology.wisc.edu"
-    subject = "Cuffdiff Report for {} vs {} : {}".format(conf['negative-condition']['label'], conf['positive-condition']['label'], time.strftime("%d/%m/%Y"))
-
-    msg['Subject'] = subject
-    msg['From'] = root
-    msg['To'] = COMMASPACE.join(options.emails)
-    msg.attach( MIMEText("\n".join(email_body)) )
-
-    # attatch the files
-    for file in [input_file, log.handlers[0].baseFilename]:
-        part = MIMEBase('application', "octet-stream")
-        part.set_payload( open(file,"rb").read() )
-        Encoders.encode_base64(part)
-        part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(file))
-        msg.attach(part)
-    
-    # Send the message via our own SMTP server, but don't include the
-    # envelope header.
-    s = smtplib.SMTP('localhost')
-    s.sendmail(root, options.emails, msg.as_string())
-    s.quit()
 
 # run the pipeline
 cmdline.run(options)
